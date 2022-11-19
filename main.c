5aa1b27236739650acfe6324b57dedf32c3ebbd5
yunushanifi84 <69470694+yunushanifi84@users.noreply.github.com>
2022-11-19 16:54:20 +0300

    Ters Dizi
    Girilen diziyi tersine çeviriyor

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int n;
+    printf("Dizi Kac Elemanli olsun:");
+    scanf("%d",&n);
+    
+    int dizi[n];
+    printf("Elemanlari girin:");
+    for (int i=0;i<n;i++){
+        scanf("%d",&dizi[n]);
+    }
+    printf("\n\nDizi Tersten Yazdiriliyor:\n");
+    for(int i=(n-1);i>=0;i--){
+        printf("\n%d",dizi[i]);
+    }
+
+
+
+
+    return 0;
+}
