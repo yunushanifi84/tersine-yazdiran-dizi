3638d8272f4b9ea6dce041e23016d467e1b7c51d
yunushanifi84 <69470694+yunushanifi84@users.noreply.github.com>
2022-11-19 16:58:26 +0300

    ufak bi fix

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(){
     int dizi[n];
     printf("Elemanlari girin:");
     for (int i=0;i<n;i++){
-        scanf("%d",&dizi[n]);
+        scanf("%d",&dizi[i]);
     }
     printf("\n\nDizi Tersten Yazdiriliyor:\n");
     for(int i=(n-1);i>=0;i--){
